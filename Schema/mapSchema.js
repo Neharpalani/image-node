550c91a78455fd86d0d8351445e775963286146c
Neha <nehaharpalani087@gmail.com>
2023-11-01 10:43:09 +0530

    neha node commit

--- Schema/mapSchema.js
+++ Schema/mapSchema.js
@@ -4,7 +4,15 @@ const Schema = mongoose.Schema;
 const mapSchema=mongoose.Schema({
     image : {
         type:String
+    },
+    message:{
+        type:Array
+    },
+    
+    cordinates:{
+        type:Array
     }
+   
 });
 const map=mongoose.model('map',mapSchema);
 module.exports=map;
